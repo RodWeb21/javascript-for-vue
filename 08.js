2ff806df4a9e24dbf2ec5af91cf16bee40925a31
Rodrigo <rodweb21@gmail.com>
2025-04-03 16:33:03 -0500

    Array Destructuring

---
+++ 08.js
@@ -0,0 +1,15 @@
+// Destructuring de arrays
+
+const tecnologias = ['Vue.js', 'JavaScript', 'Node.js', 'HTML', 'CSS'];
+
+// Destructuring - version corta
+const [ vuejs, js, , html ] = tecnologias;
+
+// Version larga
+// const vuejs = tecnologias[0];
+// const js = tecnologias[1];
+// const html = tecnologias[3];
+
+console.log(vuejs);
+console.log(js);
+console.log(html);
