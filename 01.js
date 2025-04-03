ddb49dc8a7df1ae41b3038cd26b615557d265e87
Rodrigo <rodweb21@gmail.com>
2025-04-02 22:07:16 -0500

    Data types

---
+++ 01.js
@@ -0,0 +1,46 @@
+// Tipos de datos
+
+// Undefined
+// let cliente;
+// console.log(typeof cliente);
+
+// Boolean
+// const descuento = true;
+// console.log(typeof descuento);
+
+// Number
+// const numero1 = 20.30;
+// const numero2 = 30;
+// const numero3 = -200;
+
+// console.log(typeof numero1);
+// console.log(typeof numero2);
+// console.log(typeof numero3);
+
+// Strings o cadenas de texto
+// const alumno = 'Juan';
+// let producto = "Monitor 29 pulgadas";
+
+// console.log(typeof alumno);
+// console.log(typeof producto);
+
+// Bigint
+// const numeroGrande = BigInt(1);
+// const numero = 30;
+
+// console.log(typeof numeroGrande);
+// console.log(numero + Number(numeroGrande));
+
+// Symbols
+// const primerSymbol = Symbol(1);
+// const segundoSymbol = Symbol(1);
+
+// console.log(primerSymbol === segundoSymbol);
+
+// Object
+const numeros = [1, 2, 3];
+const descuento = null
+const alumno = { }
+console.log(typeof numeros);
+console.log(typeof descuento);
+console.log(typeof alumno);
