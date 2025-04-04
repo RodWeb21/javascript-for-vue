24769638592292c442e3947f77da27894a3057c1
Rodrigo <rodweb21@gmail.com>
2025-04-04 10:28:07 -0500

    Ternary operator

---
+++ 17.js
@@ -0,0 +1,7 @@
+// Operador ternario - condicion ? si se cumple : si no se cumple
+
+const disponible = 100;
+const total = 500;
+const tarjeta = false;
+
+disponible > total || tarjeta ? console.log('Si puedes pagar con saldo o tarjeta') : console.log('No puedes pagar');
